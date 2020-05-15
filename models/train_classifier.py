2c281a5139cc7291f40cd3d12d4285140bacbf2f
Franky007Bond <frank.merwerth@gmx.de>
2020-05-15 14:21:02 +0200

    feat: GridSearch and additional feature added to ML pipeline
    ML Pipeline contains now the additonal feature "contains cardinal number"
    and SVC as classifier
    GridSearch is used for Hyperparamter tuning
    
    Changes to be committed:
    	modified:   models/train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -3,22 +3,24 @@ import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
 import nltk
-nltk.download(['punkt', 'wordnet', 'stopwords'])
+nltk.download(['punkt', 'wordnet', 'stopwords', 'averaged_perceptron_tagger'])
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.multioutput import MultiOutputClassifier
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import classification_report
 import pickle
 
 def load_data(database_filepath):
     """
-        Loads the Disaster Reponse messages and categories from SQL databse
-        created by ETL-pipeline as defined in process_data.py  
+        Loads the Disaster Reponse messages and categories from an SQL databse
+        created by an ETL-pipeline as defined in process_data.py  
         
         Args:
             database_filepath   str                 filepath to SQL databse
@@ -26,7 +28,7 @@ def load_data(database_filepath):
         Returns:
             X                   pandas.DataFrame    messages extracted from SQL database
             Y                   pandas.Datafrane    category labels from SQL database
-            category_names      List of str         Names of category labels    
+            category_names      list of str         names of category labels    
     """
     # load data from database
     engine = create_engine('sqlite:///{}'.format(database_filepath))
@@ -41,7 +43,7 @@ def load_data(database_filepath):
 def tokenize(text):
     """
         Transforms the text into a list of cleaned tokens
-        The cleaning consists of: 
+        Cleaning consists of: 
         - splitting text into tokens
         - removing stopwords
         - case normalizing 
@@ -69,26 +71,71 @@ def tokenize(text):
     return clean_tokens
 
 
+class ContainingNumberExtractor(BaseEstimator, TransformerMixin):
+    """
+        Class to apply the additional feature "contains cardinal number"
+        in the ML pipeline
+    """
+
+    def containing_number(self, text): 
+        """
+            Function too apply additional Feature
+            
+            Args:
+                text     str
+            
+            Returns
+               boolean   True in case text contains cardinal number
+        """
+        pos_tags = nltk.pos_tag(tokenize(text))
+        for word, tag in pos_tags:
+            if tag == 'CD':
+                return True
+        return False
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.containing_number)
+        return pd.DataFrame(X_tagged)
+
+
+
 def build_model():
     """
         Creates the ML model to classify disaster responses
-        Model is created in form of a pipeline the combine
-        CountVectorizer, TfidfTransofmer and the classifier
+        Model is created in form of a pipeline that combines
+        CountVectorizer, TfidfTransofmer, the additional feature
+        ContainingNumberExtractor() and SVC as classifier
+        The Model uses GridSearch for Hyperparamter tuning
         
         Args:
             None
             
         Returns
-            pipeline       Multiclassification model        build model
+            pipeline    Multiclassification model    built model
     """
     pipeline = Pipeline([
-                    ('vect', CountVectorizer(tokenizer=tokenize)),
-                    ('tfidf', TfidfTransformer()),
-                    ('clf', MultiOutputClassifier(RandomForestClassifier()))    
-                ])
+      ('features', FeatureUnion([
+          ('text_pipeline', Pipeline([
+              ('vect', CountVectorizer(tokenizer=tokenize)),
+              ('tfidf', TfidfTransformer())                                     
+          ])),
+          ('containing_number', ContainingNumberExtractor())                                          
+      ])),
+      ('clf', MultiOutputClassifier(OneVsRestClassifier(SVC())))
+    ])   
     
-    return pipeline
-
+    params_svc = {
+        'features__text_pipeline__vect__ngram_range': ((1, 1), (1, 2)),
+        'features__text_pipeline__tfidf__norm': ['l1', 'l2'],
+        'clf__estimator__estimator__kernel': ['linear', 'rbf'],
+    }
+    
+    cv = GridSearchCV(pipeline, param_grid=params_svc, verbose=3, n_jobs=3)
+    
+    return cv
 
 def evaluate_model(model, X_test, Y_test, category_names):
     """
